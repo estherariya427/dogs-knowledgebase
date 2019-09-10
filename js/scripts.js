732112b2e83e095c77656e80e46583cbf58451f7
Esther <estherariya427@gmail.com>
2019-09-10 14:38:32 -0700

    changed to food api instead of dog

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,72 @@
-var dogRepository = (function () {
+var foodRepository = (function () {
   var repository = [];
+  var apiUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
   function loadList() {
-    return $.ajax('https://dog.ceo/api/breeds/list/all', { dataType: 'json'}).then(function (responseJSON) {
+    $.ajax(apiUrl, { dataType: 'json'}).done(function (responseJSON) {
+      console.log("success");
       console.log(responseJSON);
-    }).then(function (data) {
-      var dog = {
-        name: $(this).data;
-      };
-      add(dog);
-    });
-  }).catch(function (e) {
-      console.error(e);
+      var i = 0;
+      var keys = Object.keys(responseJSON.meals[i]);
+      console.log(keys);
+      for (var j in keys) {
+        var food = {
+          name: keys[j].strMeal
+        }
+      }
+      console.log(food);
+      add(food);
+    }).catch(function (e) {
+      console.log(e);
     });
   }
 
-function loadImage(item) {
-  return $.ajax('https://dog.ceo/api/breed/' + item + '/images/random', )
-}
-
-  function add(dog) {
-    if(typeof dog === typeof {}) {
-      repository.push(dog);
+  function add(food) {
+    //check if the parameter is an Object
+    if(typeof food === typeof {}) {
+      repository.push(food);
     }
     else {
-      return document.write('<p>"' + dog +'" is a wrong input type! </p>');
-    }
+     return document.write('<p> "' + food + '" is a wrong input type! </p>');
+   }
   }
 
-  function addListItem(dog) {
-    var $element = $().
+  // function that returns the entire food repository
+  function getAll() {
+    console.log(repository);
+    return respository;
+  }
+
+  function addListItem(food) {
+    consolelog("in addListItem");
+    // create list element
+    var newListItem = document.createElement('li');
+    newListItem.classList.add('food-item');
+    newListItem.innerText = '';
+
+    // create button element and add name to innerText
+    var newButtonItem = document.createElement('button');
+    newButtonItem.innerText = food.name;
+
+    // append button to list element
+    newListItem.appendChild(newButtonItem);
+
+    // select the unordered list in the DOM and append list item
+    var $foodList = document.querySelector('food-list');
+    $foodList.appendChild(newListItem);
   }
 
   return {
     loadList: loadList,
-    add: add
-  };
+    add: add,
+    getAll: getAll,
+    addListItem: addListItem
+  }
+
 })();
 
-dogRepository.loadList().then(function() {
-
-
+foodRepository.loadList().then(function() {
+  foodRepository.getAll().forEach(function(food) {
+    foodRepository.addListItem(food);
+  });
 });
