42fcb8e71ea9d9de37a6a1ea9b4f40f7d9407b18
Esther <estherariya427@gmail.com>
2019-09-10 18:20:14 -0700

    Updated scripts.js
    added Modal functions

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,9 @@ var foodRepository = (function () {
       for (var i = 0; i < data.length; i++) {
         var obj = responseJSON.meals[i];
         var food = {
-          name: obj.strMeal
-        }
+          name: obj.strMeal,
+          imageUrl: obj.strMealThumb
+        };
         add(food);
         console.log(food);
       }
@@ -31,12 +32,10 @@ var foodRepository = (function () {
 
   // function that returns the entire food repository
   function getAll() {
-    console.log(repository);
-    return respository;
+    return repository;
   }
 
   function addListItem(food) {
-    console.log("in addListItem");
     // create list element
     var newListItem = document.createElement('li');
     newListItem.classList.add('food-item');
@@ -45,26 +44,87 @@ var foodRepository = (function () {
     // create button element and add name to innerText
     var newButtonItem = document.createElement('button');
     newButtonItem.innerText = food.name;
+    newButtonItem.classList.add('buttonStyle');
 
     // append button to list element
     newListItem.appendChild(newButtonItem);
 
     // select the unordered list in the DOM and append list item
-    var $foodList = document.querySelector('food-list');
+    var $foodList = document.querySelector('.food-list');
     $foodList.appendChild(newListItem);
+
+    // Adding event Listener when click on the button
+    newButtonItem.addEventListener('click', function(event) {
+      showDetails(food);
+    });
   }
 
+  var $modalContainer = document.querySelector('#modal-container');
+  function showModal(title, image) {
+
+    $modalContainer.classList.add('is-visible');
+
+    // Clear all existing modal content
+    $modalContainer.innerHTML = '';
+
+    var modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content: Name, height, and image
+    var closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    var titleElement = document.createElement('h1');
+    titleElement.innerText = title;
+
+    var imageElement = document.createElement('img');
+    imageElement.src = image;
+    imageElement.classList.add('myImage');
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(titleElement);
+    modal.appendChild(imageElement);
+    $modalContainer.appendChild(modal);
+
+    $modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    $modalContainer.classList.remove('is-visible');
+  }
+
+  function showDetails(food) {
+    showModal(food.name, food.imageUrl);
+  }
+
+  window.addEventListener('keydown', (e) => {
+    if(e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  $modalContainer.addEventListener('click', (e) => {
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
   return {
     loadList: loadList,
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    showDetails: showDetails,
+    showModal: showModal,
+    hideModal: hideModal
   }
 
 })();
 
 foodRepository.loadList().then(function() {
-  foodRepository.getAll().forEach(function(food) {
+  foodRepository.getAll().forEach(function (food) {
     foodRepository.addListItem(food);
   });
 });
